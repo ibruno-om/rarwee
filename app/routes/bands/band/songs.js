98f40687713f113edcd1ca0e8898b11493ecaebc
Iago Oliveira <ibruno.om@gmail.com>
2021-08-22 22:39:23 -0300

    First commit;

---
+++ app/routes/bands/band/songs.js
@@ -0,0 +1,9 @@
+// app/routes/bands/band/songs.js
+import Route from '@ember/routing/route';
+
+export default class BandsBandSongsRoute extends Route {
+  model() {
+    let band = this.modelFor('bands.band');
+    return band.songs;
+  }
+}
