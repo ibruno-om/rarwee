2bb93f09734dc0bb869f98e0076c4af4f9fe09a4
Iago Oliveira <ibruno.om@gmail.com>
2021-11-11 21:50:46 -0300

    Added band list component Added band navigation component Refactored code

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -9,6 +9,26 @@ export default class BandsNewController extends Controller {
 
   @tracked name;
 
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', (transition) => {
+      if (transition.isAborted || this.confirmedLeave) {
+        return;
+      }
+
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   updateName(event) {
     this.name = event.target.value;
@@ -17,6 +37,7 @@ export default class BandsNewController extends Controller {
   @action
   async saveBand() {
     let band = await this.catalog.create('band', { name: this.name });
+    this.confirmedLeave = true;
     this.router.transitionTo('bands.band.songs', band.id);
   }
 }
